90b87723fe2c33a415ebb9c57a7b564415d428f8
AltoTenor <aritro10@gmail.com>
2024-07-26 06:04:22 +0000

    Stage2 main

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -177,38 +177,3 @@ int main(int argc, char *argv[]) {
 
 //   return 0;
 // }
-
-//-----------------------------------------------------------------
-// Exercise - Stage 1
-// ------------------
-// int main(int argc, char *argv[]) {
-//   Disk disk_run;
-
-//   unsigned char buffer2[BLOCK_SIZE];
-//   Disk::readBlock(buffer2, 0);
-//   for (auto x:buffer2) printf("%d ",x);
-//   return 0;
-// }
-
-//-----------------------------------------------------------------
-
-// Stage 1 
-// ------------------
-// int main(int argc, char *argv[]) {
-//   Disk disk_run;
-
-//   unsigned char buffer[BLOCK_SIZE];
-//   Disk::readBlock(buffer, 7000);
-//   char message[] = "hello";
-//   memcpy(buffer + 20, message, 6);
-//   Disk::writeBlock(buffer, 7000);
-
-//   unsigned char buffer2[BLOCK_SIZE];
-//   char message2[6];
-//   Disk::readBlock(buffer2, 7000);
-//   memcpy(message2, buffer2 + 20, 6);
-//   std::cout << message2;
-
-//   return 0;
-// }
-//-----------------------------------------------------------------
