598ba9f7716a31db6a18a3194f2523bcc13a6a19
AltoTenor <aritro10@gmail.com>
2024-07-29 19:36:24 +0000

    Added staticBuffer op

--- mynitcbase/Buffer/BlockBuffer.cpp
+++ mynitcbase/Buffer/BlockBuffer.cpp
@@ -15,42 +15,46 @@ RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
 
 // load the block header into the argument pointer
 int BlockBuffer::getHeader(struct HeadInfo *head) {
-  unsigned char buffer[BLOCK_SIZE];
+  
+  // Use loadBlockAndGetBufferPtr to retrieve pointer to buffer
+  unsigned char *bufferPtr;
+  int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+  if (ret != SUCCESS) {
+    return ret;   // return any errors that might have occured in the process
+  }
 
-  // read the block at this.blockNum into the buffer
-  Disk::readBlock(buffer, this->blockNum );
   // populate the numEntries, numAttrs and numSlots fields in *head
-  memcpy(&head->lblock, buffer + 8, 4);
-  memcpy(&head->rblock, buffer + 12, 4);
-  memcpy(&head->numEntries, buffer + 16, 4);
-  memcpy(&head->numAttrs, buffer + 20, 4);
-  memcpy(&head->numSlots, buffer + 24, 4);
-
+  memcpy(&head->lblock, bufferPtr + 8, 4);
+  memcpy(&head->rblock, bufferPtr + 12, 4);
+  memcpy(&head->numEntries, bufferPtr + 16, 4);
+  memcpy(&head->numAttrs, bufferPtr + 20, 4);
+  memcpy(&head->numSlots, bufferPtr + 24, 4);
   return SUCCESS;
 }
 
 // load the record at slotNum into the argument pointer
 int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
+
+  // Use loadBlockAndGetBufferPtr to retrieve pointer to buffer
+  unsigned char *bufferPtr;
+  int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+  if (ret != SUCCESS) {
+    return ret;
+  }
+
   struct HeadInfo head;
-
-  // get the header using this.getHeader() function - Done
-
   this->getHeader(&head);
 
   int attrCount = head.numAttrs;
   int slotCount = head.numSlots;
 
-  // read the block at this.blockNum into a buffer - Done
-  unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer, this->blockNum );
 
   /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
      - each record will have size attrCount * ATTR_SIZE
      - slotMap will be of size slotCount
-     - Done
   */
   int recordSize = attrCount * ATTR_SIZE;
-  unsigned char *slotPointer = buffer + HEADER_SIZE + slotCount + (recordSize * slotNum);
+  unsigned char *slotPointer = bufferPtr + HEADER_SIZE + slotCount + (recordSize * slotNum);
 
   // load the record into the rec data structure - Done
   memcpy(rec, slotPointer, recordSize);
@@ -59,6 +63,29 @@ int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
 }
 
 // Update the record for now 
-int RecBuffer:: setRecord(union Attribute *rec, int slotNum){
+// int RecBuffer:: setRecord(union Attribute *rec, int slotNum){
+// }
 
+/*
+Used to load a block to the buffer and get a pointer to it.
+NOTE: this function expects the caller to allocate memory for the argument
+*/
+int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char **buffPtr) {
+  // check whether the block is already present in the buffer using StaticBuffer.getBufferNum()
+  int bufferNum = StaticBuffer::getBufferNum(this->blockNum);
+
+  if (bufferNum == E_BLOCKNOTINBUFFER) {
+    bufferNum = StaticBuffer::getFreeBuffer(this->blockNum);
+
+    if (bufferNum == E_OUTOFBOUND) {
+      return E_OUTOFBOUND;
+    }
+    // Do readblock if required block is not present in buffer
+    Disk::readBlock(StaticBuffer::blocks[bufferNum], this->blockNum);
+  }
+
+  // store the pointer to this buffer (blocks[bufferNum]) in *buffPtr
+  *buffPtr = StaticBuffer::blocks[bufferNum];
+
+  return SUCCESS;
 }
--- mynitcbase/Buffer/StaticBuffer.cpp
+++ mynitcbase/Buffer/StaticBuffer.cpp
@@ -1,1 +1,49 @@
 #include "StaticBuffer.h"
+
+unsigned char StaticBuffer::blocks[BUFFER_CAPACITY][BLOCK_SIZE];
+struct BufferMetaInfo StaticBuffer::metainfo[BUFFER_CAPACITY];
+
+StaticBuffer::StaticBuffer() {
+
+  // initialise all buffers as free
+  for (int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++ ) {
+    metainfo[bufferIndex].free = true;
+  }
+
+}
+
+// yet to perform write back
+StaticBuffer::~StaticBuffer() {}
+
+
+int StaticBuffer::getFreeBuffer(int blockNum) {
+  if (blockNum < 0 || blockNum > DISK_BLOCKS) {
+    return E_OUTOFBOUND;
+  }
+
+  // Get free buffer index 
+  int allocatedBuffer;
+  for (allocatedBuffer = 0; allocatedBuffer < BUFFER_CAPACITY; allocatedBuffer++ ){
+    if ( metainfo[allocatedBuffer].free = true ) break;
+  }
+
+  metainfo[allocatedBuffer].free = false;
+  metainfo[allocatedBuffer].blockNum = blockNum;
+
+  return allocatedBuffer;
+}
+
+
+// Retrieve index of Block stored in buffer already
+int StaticBuffer::getBufferNum( int blockNum ){
+  if (blockNum < 0 || blockNum > DISK_BLOCKS) {
+    return E_OUTOFBOUND;
+  }
+  int bufferIndex;
+  for (bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++ ){
+    if ( metainfo[bufferIndex].blockNum = blockNum ) break;
+  }
+
+  if ( bufferIndex == BUFFER_CAPACITY ) return E_BLOCKNOTINBUFFER;
+  else return bufferIndex;
+}
