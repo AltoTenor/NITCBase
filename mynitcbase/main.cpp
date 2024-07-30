ffc1f90bf1d242e403e1d19397c88bbd3e107299
AltoTenor <aritro10@gmail.com>
2024-07-30 04:04:48 +0000

    stage 2 ex 2 done

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -14,12 +14,60 @@ int main(int argc, char *argv[]) {
 
   // create objects for the relation catalog and attribute catalog
   RecBuffer relCatBuffer(RELCAT_BLOCK);
-
   HeadInfo relCatHeader;
-
   // load the headers of both the blocks into relCatHeader and attrCatHeader - Done
   relCatBuffer.getHeader(&relCatHeader);
 
+
+  for (int i = 0; i < relCatHeader.numEntries ; i++ ) {
+
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    // Loop through all possible attrCat Blocks
+    int blockNum = ATTRCAT_BLOCK;
+    while( true ) {
+
+      RecBuffer attrCatBuffer(blockNum);
+      HeadInfo attrCatHeader;
+      attrCatBuffer.getHeader(&attrCatHeader);
+
+      for (int j = 0; j < attrCatHeader.numEntries; j++ ) {
+
+        // declare attrCatRecord and load the attribute catalog entry into it
+        Attribute attrCatRecord[RELCAT_NO_ATTRS];
+        attrCatBuffer.getRecord(attrCatRecord, j);
+
+        if ( strcmp( attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, 
+                      relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0 &&
+             strcmp(relCatRecord[RELCAT_REL_NAME_INDEX].sVal, "Students") == 0 ){
+
+          const char *attrType = 
+            attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+
+          // printf(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal);
+          if ( strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,"Batch") == 0 ){
+            unsigned char buffer[BLOCK_SIZE];
+            // read the block at this.blockNum into a buffer
+            Disk::readBlock(buffer,ATTRCAT_BLOCK);
+            int slotCount = attrCatHeader.numSlots;
+            int attrCount = attrCatHeader.numAttrs;
+            int recordSize = attrCount * ATTR_SIZE;
+            unsigned char *slotPointer = buffer+32+slotCount+(recordSize*j)+16;
+            char new_class[]="Class";
+            
+            memcpy(slotPointer,new_class,16);
+            Disk::writeBlock(buffer,ATTRCAT_BLOCK);
+          }
+        }
+        
+      }
+      // printf("%d\n", attrCatHeader.rblock);
+      if ( attrCatHeader.rblock == -1 ) break;
+      else blockNum = attrCatHeader.rblock;
+    }
+  }
 
   for (int i = 0; i < relCatHeader.numEntries ; i++ ) {
 
@@ -60,6 +108,7 @@ int main(int argc, char *argv[]) {
 
     printf("\n");
   }
+
 
   return 0;
 }
